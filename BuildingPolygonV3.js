f4823f5c94b34e111f4a6fc037c6a866908390c3
Tya Meilisa <117277043+tyameilisa@users.noreply.github.com>
2025-11-07 10:07:54 +0700

    Create BuildingPolygonV3.js

---
+++ BuildingPolygonV3.js
@@ -0,0 +1,21 @@
+// Load your region (uploaded shapefile)
+var region = ee.FeatureCollection('projects/ee-meilisa1146/assets/08');
+
+// Load the Open Buildings v3 polygons
+var buildings = ee.FeatureCollection('GOOGLE/Research/open-buildings/v3/polygons')
+                    .filterBounds(region);
+
+// Check how many buildings exist
+print('Number of building polygons in region:', buildings.size());
+
+// Visualize on map
+Map.centerObject(region, 13);
+Map.addLayer(region.style({color: 'red', fillColor: '00000000'}), {}, 'Region Boundary');
+Map.addLayer(buildings.limit(5000), {color: 'blue'}, 'Buildings sample');  // limit for display
+
+// Export to Drive as Shapefile
+Export.table.toDrive({
+  collection: buildings,
+  description: 'OpenBuildings_v3_IndonesiaRegion',
+  fileFormat: 'SHP'
+});
